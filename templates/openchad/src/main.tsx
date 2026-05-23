f2492ddc8dd68971de1cd7689486afe9b1fdb051
Hafidz Ihza Pratama <contact@antitesa.com>
2026-05-23 09:28:50 +0700

    auto apply project name to frontend

--- templates/openchad/src/main.tsx
+++ templates/openchad/src/main.tsx
@@ -5,7 +5,7 @@ import App from './App'
 import { Container, OpenChadIcon, type Project } from "openchad-react"
 
 const Apps: Project = {
-  projectName: "OpenChad",
+  projectName: "{{PROJECT_NAME}}",
   projectIcon: OpenChadIcon,
   defaultTab: {
     layout: "horizontal",
